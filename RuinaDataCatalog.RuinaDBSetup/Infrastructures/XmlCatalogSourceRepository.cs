ddd9c558536e38c48a6bd11181a67caa4c4a0f8a
Tana <finaltruth.shintai@gmail.com>
2022-05-26 23:31:39 +0900

    元データXMLの読み取りをメソッドチェーンに置き換えて汎用化。

--- RuinaDataCatalog.RuinaDBSetup/Infrastructures/XmlCatalogSourceRepository.cs
+++ RuinaDataCatalog.RuinaDBSetup/Infrastructures/XmlCatalogSourceRepository.cs
@@ -12,15 +12,12 @@ namespace RuinaDataCatalog.RuinaDBSetup.Infrastructures;
 /// </summary>
 public class XmlCatalogSourceRepository : ICatalogSourceRepository
 {
-    /// <summary>XML ファイルの読込で使用する文字エンコーディング</summary>
-    private readonly Encoding _encoding = Encoding.UTF8;
     /// <summary>読み込んだバトル ページ情報のリスト</summary>
     private readonly List<DiceCardXmlInfo> _cards = new();
     /// <summary>読み込んだバトル ページ説明のリスト</summary>
     private readonly List<BattleCardDesc> _cardDescriptions = new();
 
     public IEnumerable<DiceCardXmlInfo> Cards => _cards;
-
     public IEnumerable<BattleCardDesc> CardDescriptions => _cardDescriptions;
 
     /// <summary>
@@ -31,41 +28,146 @@ public class XmlCatalogSourceRepository : ICatalogSourceRepository
     {
         if (sourceDirectoryPath == null) { throw new ArgumentNullException(nameof(sourceDirectoryPath)); }
 
-        foreach(FileInfo file in EnumerateFiles(sourceDirectoryPath))
+        Console.WriteLine("元データを読み込みます。");
+
+        foreach (string sourceFilePath in EnumerateSourceFiles(sourceDirectoryPath))
         {
             try
             {
-                Console.Write($"'{file.FullName}' を読み込みます... ");
+                Console.WriteLine($"- '{sourceFilePath}'");
 
-                string? rootName = XDocument.Load(file.FullName).Root?.Name.LocalName;
-
-                switch (rootName)
+                string rootName = LoadRootElementName(sourceFilePath);
+                LoadingResult result = rootName switch
                 {
-                    case null:
-                        var serializer = new XmlSerializer(typeof(DiceCardXmlRoot));
-                        Console.WriteLine("エラーのため読み飛ばしました <ルート要素なし>");
-                        continue;
-                    default:
-                        Console.WriteLine($"読込対象外のルート要素のため読み飛ばしました <{rootName}>");
-                        continue;
-                }
+                    "DiceCardXmlRoot" => LoadFrom<DiceCardXmlRoot>(sourceFilePath).SelectItems(r => r.cardXmlList).AddTo(ref _cards),
+                    "BattleCardDescRoot" => LoadFrom<BattleCardDescRoot>(sourceFilePath).SelectItems(r => r.cardDescList).AddTo(ref _cardDescriptions),
+                    _ => LoadingResult.UnknownRoot,
+                };
 
-                Console.WriteLine("完了");
+                Console.WriteLine($"  - {result}");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"エラーのため読み飛ばしました <{ex.GetType().Name}>");
+                Console.WriteLine($"  - {ex.Message}");
                 throw;
             }
         }
+
+        Console.WriteLine("読み込みが完了しました。");
+        Console.WriteLine();
     }
 
-    private static IEnumerable<FileInfo> EnumerateFiles(string path)
-    {
-        var dir = new DirectoryInfo(path);
-        return dir
+    /// <summary>
+    /// 指定したフォルダーとサブ フォルダーに含まれる元データ XML ファイルの絶対パスを列挙します。
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    private static IEnumerable<string> EnumerateSourceFiles(string path)
+        => new DirectoryInfo(path)
             .EnumerateFiles("*", SearchOption.AllDirectories)
             .Where(f => f.Extension == ".txt" || f.Extension == ".xml")
+            .Select(f => f.FullName)
             .ToArray();
+
+    /// <summary>
+    /// 指定した XML ファイルからルート要素のローカル名を読み込んで返します。
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static string LoadRootElementName(string path)
+    {
+        try
+        {
+            return XDocument.Load(path).Root?.Name.LocalName
+                ?? throw new ApplicationException("ルート要素がありません。");
+        }
+        catch (ApplicationException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("XML ファイルとして読み込みできません。", ex);
+        }
     }
+
+    #region 「XML の読込→ルート要素から対象データの選択→リストへ追加」を行うメソッド チェーンの定義
+
+    /// <summary>
+    /// 指定した元データ XML ファイルを読み込み、そのルート要素を格納した結果を返します。
+    /// </summary>
+    /// <typeparam name="TXmlRoot"></typeparam>
+    /// <param name="sourceFilePath"></param>
+    /// <returns></returns>
+    private LoadedRootResult<TXmlRoot> LoadFrom<TXmlRoot>(string sourceFilePath) where TXmlRoot : class
+    {
+        var serializer = new XmlSerializer(typeof(TXmlRoot));
+        using var reader = new StreamReader(sourceFilePath, Encoding.UTF8);
+        var root = serializer.Deserialize(reader) as TXmlRoot ?? throw new InvalidCastException();
+        return new LoadedRootResult<TXmlRoot>(root);
+    }
+
+    private class LoadedRootResult<TXmlRoot>
+    {
+        private readonly TXmlRoot _root;
+
+        public LoadedRootResult(TXmlRoot root)
+            => _root = root;
+
+        /// <summary>
+        /// 読み込んだルート要素からリストに追加する要素のコレクションを選択し、その要素のコレクションを格納した結果を返します。
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public SelectedItemsResult<TItem> SelectItems<TItem>(Func<TXmlRoot, IEnumerable<TItem>> selector)
+            => new SelectedItemsResult<TItem>(selector.Invoke(_root));
+    }
+
+    private class SelectedItemsResult<TItem>
+    {
+        private readonly IEnumerable<TItem> _items;
+
+        public SelectedItemsResult(IEnumerable<TItem> items)
+            => _items = items;
+
+        /// <summary>
+        /// 選択した要素のコレクションを指定したリストに追加します。
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public LoadingResult AddTo(ref List<TItem> list)
+        {
+            list.AddRange(_items);
+            return new LoadingResult(typeof(TItem).Name , _items.Count());
+        }
+    }
+
+    /// <summary>
+    /// 元データ XML ファイルを読み込んだ結果を格納します。
+    /// </summary>
+    private class LoadingResult
+    {
+        public static readonly LoadingResult UnknownRoot = new();
+
+        private readonly string _typeName;
+        private readonly int _loadedCount;
+
+        public LoadingResult(string typeName, int loadedCount)
+        {
+            _typeName = typeName;
+            _loadedCount = loadedCount;
+        }
+
+        private LoadingResult() : this("", 0) { }
+
+        public override string ToString()
+        {
+            if (this == UnknownRoot) { return "読込対象外のルート要素です。"; }
+
+            return $"読み込んだデータ数: {_loadedCount} <{_typeName}>";
+        }
+    }
+
+    #endregion
 }
