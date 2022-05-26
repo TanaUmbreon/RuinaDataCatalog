5091180347c891344732f56a3c64044d6a64f2b4
Tana <finaltruth.shintai@gmail.com>
2022-05-27 01:34:30 +0900

    XmlCatalogSourceRepositoryのテストケースを追加。

---
+++ RuinaDataCatalog.RuinaDBSetup.Test/Infrastructures/XmlCatalogSourceRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using RuinaDataCatalog.RuinaDBSetup.Infrastructures;
+
+namespace RuinaDataCatalog.RuinaDBSetup.Test.Infrastructures;
+
+[TestFixture]
+public class XmlCatalogSourceRepositoryTest
+{
+    [Test]
+    public void TestNew_ThorwsArgumentNullException()
+    {
+        Assert.That(() => new XmlCatalogSourceRepository(null!),
+            Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void TestNew_ThrowsDirectoryNotFound()
+    {
+        Assert.That(() => new XmlCatalogSourceRepository(@"存在しないフォルダー\"),
+            Throws.Exception.TypeOf<DirectoryNotFoundException>());
+    }
+
+    [Test]
+    public void TestNew()
+    {
+        var repo = new XmlCatalogSourceRepository(@"TestInputData\");
+        Assert.That(repo.Cards.Count(), Is.EqualTo(3));
+    }
+}
